2cae954c3ec15d0f67160579bb299499db942143
wyyoon <wonyoung5488@hanmail.net>
2020-05-13 09:22:31 +0900

    csv 파일 불러오기

--- src/main/java/com/jojoldu/book/springboot/service/PostsService.java
+++ src/main/java/com/jojoldu/book/springboot/service/PostsService.java
@@ -1,15 +1,18 @@
 package com.jojoldu.book.springboot.service;
 
 import com.jojoldu.book.springboot.domain.posts.Posts;
+import com.jojoldu.book.springboot.domain.posts.PostsExcelRepository;
 import com.jojoldu.book.springboot.domain.posts.PostsRepository;
-import com.jojoldu.book.springboot.web.dto.PostsListResponseDto;
-import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
-import com.jojoldu.book.springboot.web.dto.PostsUpdateRequestDto;
-import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
+import com.jojoldu.book.springboot.web.dto.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,6 +20,7 @@ import java.util.stream.Collectors;
 @Service
 public class PostsService {
     private final PostsRepository postsRepository;
+    private final PostsExcelRepository postsExcelRepository;
 
     @Transactional
     public Long save(PostsSaveRequestDto requestDto){
@@ -45,5 +49,44 @@ public class PostsService {
     public void delete(Long id){
         Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
         postsRepository.delete(posts);
+    }
+
+    public void insertExcel(){
+        BufferedReader br = null;
+
+        try{
+            File csv = new File("/Users/closetshare-dev/Downloads/posts.csv");
+            br = new BufferedReader(new FileReader(csv));
+            String line = "";
+
+            int index = 0;
+            while((line = br.readLine()) != null){
+                if( 0 < index){
+                    List<String> tmpList = new ArrayList<String>();
+                    String array[] = line.split(",");
+                    tmpList = Arrays.asList(array);
+
+                    System.out.println("title : "+tmpList.get(0));
+                    System.out.println("content : "+tmpList.get(1));
+                    System.out.println("author : "+tmpList.get(2));
+
+                }
+
+                index++;
+            }
+
+        }catch(FileNotFoundException e){
+            e.printStackTrace();
+        }catch(IOException e){
+            e.printStackTrace();
+        }finally{
+            try{
+                if(br != null){
+                    br.close();
+                }
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+        }
     }
 }
